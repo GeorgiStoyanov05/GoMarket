058154201d211cacef99a7c0e9223755a9a2287b
GeorgiStoyanov05 <120947467+GeorgiStoyanov05@users.noreply.github.com>
2026-01-24 16:28:42 +0200

    Database connected

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	database "github.com/GeorgiStoyanov05/GoMarket2/database"
+	//routes "github.com/GeorgiStoyanov05/GoMarket2/routes"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	port := os.Getenv("PORT")
 
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	//routes.AuthRoutes(router)
+	//routes.UserRoutes(router)
+
+	database.DBInstance()
+
+	router.Run(":" + port)
 }
